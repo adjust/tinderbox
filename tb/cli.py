ee5d428eab6984460455680067144693a3a2e68c
Patrice Clement <patrice.clement@adjust.com>
2022-10-11 23:33:50 +0200

    tb/cli.py: create Tinderbox class
    assign a function to each keyword

--- tb/cli.py
+++ tb/cli.py
@@ -1,14 +1,16 @@
-"""the adjust tinderbox
+import logging
+import pydoc
+import sys
 
-Usage:
-  tinderbox
-  tinderbox (-h | --help)
-  tinderbox --version
+import docopt
 
-Options:
-    -h|--help     display help.
-    --version     display version.
-"""
+import tb.backend as tbb
+
+
+logging.basicConfig(
+    level=logging.INFO, format="[%(asctime)s][%(name)s][%(levelname)s]: %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
 __version__ = "0.0.1"
@@ -18,28 +20,45 @@ __date__ = "30/09/2022"
 __credits__ = "adjust OS team"
 
 
-import logging
-import pydoc
-import sys
+class Tinderbox:
+    """tinderbox: a Python script to drive the binary package compilation process.
 
-import docopt
+Usage:
+  tinderbox [-h|--help] [-v|--version] <command>
 
-import tb.backend as tbb
+Commands:
+  tinderbox clean_targets   clean targets directory.
+  tinderbox create_targets  create targets directory.
+  tinderbox prepare         compile base packages for all targets.
+  tinderbox compile         compile system and world packages for all targets.
+  tinderbox all             all of the above.
 
-logging.basicConfig(
-    level=logging.INFO, format="[%(asctime)s][%(name)s][%(levelname)s]: %(message)s"
-)
-logger = logging.getLogger(__name__)
+Options:
+    -h,  --help        display help.
+    -v,  --version     display version."""
 
+    def __init__(self):
+        args = docopt.docopt(
+            Tinderbox.__doc__, version=__version__, help=True, options_first=True
+        )
 
-def main():
-    """Main function."""
-    argv = docopt.docopt(__doc__)
-    logger.info("Running tinderbox with the following options:")
-    logger.info(dict(argv))
-    tbb.run()
-    return 0
+        runner = {
+            "clean_targets": tbb.clean_targets,
+            "create_targets": tbb.create_targets,
+            "prepare": tbb.prepare,
+            "compile": tbb.compile_packages,
+            "all": tbb.run,
+        }
+
+        try:
+            runner[args["<command>"]]()
+        except KeyError:
+            sys.exit(
+                """Error! \'%s\' is an invalid command!
+See 'tinderbox --help' for a complete list of commands."""
+                % (args["<command>"])
+            )
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(Tinderbox())
